410d7026f825e695b1126419dccedad714efe86f
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2024-06-24 17:21:06 -0500

    8.3 Added DatePickerViewModelTest (#14)

---
+++ common/android-test/src/main/java/com/ivy/common/androidtest/MainCoroutineRule.kt
@@ -0,0 +1,26 @@
+@file:OptIn(ExperimentalCoroutinesApi::class)
+
+package com.ivy.common.androidtest
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.TestDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.setMain
+import org.junit.rules.TestWatcher
+import org.junit.runner.Description
+
+
+class MainCoroutineRule(
+    val testDispatcher: TestDispatcher = StandardTestDispatcher()
+) : TestWatcher() {
+
+    override fun starting(description: Description?) {
+        Dispatchers.setMain(testDispatcher)
+    }
+
+    override fun finished(description: Description?) {
+        Dispatchers.resetMain()
+    }
+}
